33466d2104ac8f396d6790bfe7f981aef3fa8db7
Николай <nikola16122006@yandex.ru>
2026-05-09 22:36:18 +0300

    feat(server): создать роут для клиентов и эндпоинт для получения всех

---
+++ packages/server/src/routers/clients.ts
@@ -0,0 +1,15 @@
+import { router, protectedProcedure } from "../trpc";
+
+
+export const ClientsRouter = router({
+  getAll: protectedProcedure.query(async ({ ctx }) => {
+    return ctx.prisma.client.findMany({
+        select: {
+            id: true,
+            name: true,
+            phone: true,
+            email: true,
+        }
+    })
+  }),
+});
