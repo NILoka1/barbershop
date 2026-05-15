980dead5a3710ebc9a70db596ec3b927bbbe4cfd
Николай <nikola16122006@yandex.ru>
2026-05-15 19:52:29 +0300

    fix(client): исправить выкидывание с аккаунта при обновлении страницы

--- packages/client/src/components/ProtectedRoute.tsx
+++ packages/client/src/components/ProtectedRoute.tsx
@@ -3,7 +3,7 @@ import { Navigate, Outlet } from "react-router-dom";
 import { trpc } from "src/api/client";
 
 export function ProtectedRoute() {
-  const token = trpc.auth.me.useQuery().data;
+  const token = trpc.auth.me.useQuery().data || localStorage.getItem("token");
 
   if (!token) {
     return <Navigate to="/" replace />;
